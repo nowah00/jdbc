6b04dd874c9dfc2c1a01581696c79b9f2e114431
nowah00 <hawon2k@gmail.com>
2025-09-05 12:39:58 +0900

    commit

--- src/jebcEx01/BoardInsertTest.java
+++ src/jebcEx01/BoardInsertTest.java
@@ -1,47 +1,62 @@
-package jebcEx01;
+package jdbcEx01;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.io.FileInputStream;
+import java.sql.*;
 
 public class BoardInsertTest {
     public static void main(String[] args) {
-        String DriverName = "com.mysql.cj.jdbc.Driver";
+        String DriverName ="com.mysql.cj.jdbc.Driver";
         String url = "jdbc:mysql://localhost:3306/bookmarketdb?serverTimezone=Asia/Seoul";
         String username = "root";
         String password = "mysql1234";
 
+
+
         try{
             Class.forName(DriverName);
-            System.out.println("Driver loaded Successfully!");
+            System.out.println("Driver loaded OK!");
         } catch (Exception e) {
-            System.out.println("Driver loaded failed.");
+            System.out.println("Driver loaded failed!");
         }
+        try(Connection con = DriverManager.getConnection(url, username, password)){  //도로연결
+            System.out.println("AutoCommit 상태: " + con.getAutoCommit());
+            con.setAutoCommit(true);
 
-        try {
-            Connection con = DriverManager.getConnection(url,username,password);
+            // 매개변수화된 SQL문
+            String sql = "INSERT INTO boards(btitle,bcontent,bwriter,bdate,bfilename,bfiledata) values(?,?,?,now(),?,?)";
 
-            String sql = "insert into users values(?,?,?,?,?)";
+            //PreparedStatement 얻기
+            PreparedStatement pstmt = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
 
-            PreparedStatement ps = con.prepareStatement(sql);
+            //값 지정
+            pstmt.setString(1, "봄이 오나 봄봄1111");
+            pstmt.setString(2,"꽃이 동산에 가득하니 행복하네요1111");
+            pstmt.setString(3,"신세계111");
+            pstmt.setString(4,"spring.jpg");
+            pstmt.setBlob(5,new FileInputStream("C:/Temp/spring.jpg"));
 
-            ps.setString(1,"20");
-            ps.setString(2,"고하원");
-            ps.setString(3,"hawon");
-            ps.setString(4,"26");
-            ps.setString(5,"hw@gmail.com");
+            //SQL문 실행
+            int result = pstmt.executeUpdate();
+            System.out.println("저장된 행의 수" + result);
 
-            int result = ps.executeUpdate();
-            System.out.println("저장된 행의 수 " + result);
-
-            if (result == 0) {
-                System.out.println("Insert Successful!.");
-
-
+            //bno 값 얻기
+            if(result == 1){
+                ResultSet rs = pstmt.getGeneratedKeys();
+                if(rs.next()){
+                    int bno = rs.getInt(1);
+                    System.out.println("bno = " + bno);
+                }
+                rs.close();
             }
-        } catch (SQLException e) {
-            System.out.println("Connection established.");
+
+            if (result == 1) {
+                System.out.println("Insert successful!");
+            }
+
+
+        } catch (Exception e) {
+            System.out.println("Connection established!" + e);
         }
+
     }
 }
